b017d382cea822ed8190759a44af1bfe06d21536
Dipak bhagat <bhagatdipak25@gmail.com>
2026-03-13 17:39:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,61 @@ function generate(){
 
 var topic = document.getElementById("topic").value;
 
-var title = "🔥 " + topic + " | Viral Video";
+if(topic==""){
+alert("Please enter topic");
+return;
+}
 
-var desc = "Watch this amazing video about " + topic;
+/* Titles */
+var titles = [
+"🔥 " + topic + " | Viral Video",
+topic + " | Must Watch Video",
+"Top Facts About " + topic,
+topic + " | Amazing Story",
+"Secrets of " + topic,
+topic + " | You Won’t Believe This",
+topic + " Explained in 5 Minutes",
+topic + " | Trending Video",
+"Truth About " + topic,
+topic + " | Mind Blowing Video"
+];
 
-var tags = topic + ", youtube video, trending";
+var title = titles[Math.floor(Math.random()*titles.length)];
 
-var idea = "Create an interesting video explaining " + topic;
+/* Descriptions */
+var desc = "In this video we explore " + topic +
+". This topic is trending on the internet and many people want to know about it. Watch till the end for amazing information.";
 
-var script = "Today we are talking about " + topic + ". This topic is very interesting and many people search for it on YouTube.";
+/* Tags */
+var tags = topic + ", " +
+topic + " video, " +
+topic + " youtube, " +
+"viral video, trending video, youtube growth";
 
+/* Video Idea */
+var idea = "Create an engaging video explaining interesting facts about " + topic;
+
+/* Hook */
+var hook = "You will not believe what we discovered about " + topic + "!";
+
+/* Thumbnail Text */
+var thumbnail = topic + " SHOCKING FACTS";
+
+/* Script */
+var script = "Hello everyone! Today we are talking about " + topic +
+". This topic has become very popular recently and there are many interesting things you should know.";
+
+/* Output */
 document.getElementById("title").innerText = title;
 document.getElementById("desc").innerText = desc;
 document.getElementById("tags").innerText = tags;
 document.getElementById("idea").innerText = idea;
+document.getElementById("hook").innerText = hook;
+document.getElementById("thumbnail").innerText = thumbnail;
 document.getElementById("script").innerText = script;
 
 }
+
 
 
 
