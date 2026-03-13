7e76a6c83510dca895a948d50f501bee3756705f
Dipak bhagat <bhagatdipak25@gmail.com>
2026-03-13 17:29:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-async function generate(){
+function generate(){
 
-let topic = document.getElementById("topic").value;
+var topic = document.getElementById("topic").value;
 
-let response = await fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=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",{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-contents:[{parts:[{text:"Give YouTube title and description about "+topic}]}]
-})
-});
+var title = "🔥 " + topic + " | Viral Video";
 
-let data = await response.json();
+var desc = "Watch this amazing video about " + topic;
 
-document.getElementById("title").innerText =
-data.candidates[0].content.parts[0].text;
+var tags = topic + ", youtube video, trending";
+
+document.getElementById("title").innerText = title;
+document.getElementById("desc").innerText = desc;
+document.getElementById("tags").innerText = tags;
 
 }
 
