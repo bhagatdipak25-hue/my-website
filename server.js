0bd50bc9d2199ab2949dc8a7d04f3e22654151ca
Dipak bhagat <bhagatdipak25@gmail.com>
2026-03-13 18:53:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 import express from "express";
+import fetch from "node-fetch";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 
-app.get("/", (req,res)=>{
-res.send("AI server running");
+app.get("/ai", async (req,res)=>{
+
+let topic=req.query.topic;
+
+let prompt=`Create YouTube content about ${topic}
+
+###Titles
+Give 10 viral YouTube titles
+
+###Description
+Write SEO description
+
+###Tags
+Give 20 YouTube tags
+
+###Idea
+Give video idea
+
+###Script
+Write short video script
+`;
+
+let response=await fetch("https://api.openai.com/v1/chat/completions",{
+method:"POST",
+headers:{
+"Content-Type":"application/json",
+"Authorization":"Bearer "+process.env.OPENAI_KEY
+},
+body:JSON.stringify({
+model:"gpt-4.1-mini",
+messages:[
+{role:"user",content:prompt}
+]
+})
 });
 
-app.listen(3000, ()=>{
-console.log("Server started");
+let data=await response.json();
+
+res.json(data);
+
 });
+
+app.listen(3000);
