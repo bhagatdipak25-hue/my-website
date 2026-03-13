3eae1063153f45de6cf1741aebfd4dbfe5041ff6
Dipak bhagat <bhagatdipak25@gmail.com>
2026-03-13 17:09:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ function generate(){
 
 var topic = document.getElementById("topic").value;
 
-if(topic==""){
-alert("Please enter topic");
-return;
-}
-
 var titles = [
-topic + " | Amazing Video",
-topic + " | Must Watch",
-topic + " | Viral Video",
-topic + " | Best Moments"
+"🔥 " + topic + " | You Won’t Believe This",
+topic + " | Viral Video Idea",
+topic + " | Amazing Facts",
+topic + " | Must Watch Video"
 ];
 
 var randomTitle = titles[Math.floor(Math.random()*titles.length)];
 
-var desc = "Watch this amazing video about " + topic + ". Like and subscribe.";
+var desc = "In this video we explore " + topic + ". Watch till the end for amazing insights.";
 
-var tags = topic + ", youtube video, trending video";
+var tags = topic + ", viral video, trending video, youtube growth";
+
+var idea = "Create a video showing interesting facts about " + topic;
+
+var script = "Today we are talking about " + topic + ". This topic is very interesting and many people want to know about it.";
 
 document.getElementById("title").innerText = randomTitle;
 document.getElementById("desc").innerText = desc;
 document.getElementById("tags").innerText = tags;
+document.getElementById("idea").innerText = idea;
+document.getElementById("script").innerText = script;
 
 }
-
 
