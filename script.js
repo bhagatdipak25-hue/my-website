aba297641e107a6f079821a504ad21dbabb5a2d1
Dipak bhagat <bhagatdipak25@gmail.com>
2026-03-13 18:53:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,16 @@ let res=await fetch("https://ai-server-gukc.onrender.com/ai?topic="+topic);
 
 let data=await res.json();
 
-document.getElementById("title").innerText=data.choices[0].message.content;
+let text=data.choices[0].message.content;
 
-}
+// response split
+let parts=text.split("###");
+
+document.getElementById("title").innerText=parts[1];
+document.getElementById("desc").innerText=parts[2];
+document.getElementById("tags").innerText=parts[3];
+document.getElementById("idea").innerText=parts[4];
+document.getElementById("script").innerText=parts[5];
 
 }
 
