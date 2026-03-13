fcf2a6731a4ee9a9a06efeac06f137699d17d16d
Dipak bhagat <bhagatdipak25@gmail.com>
2026-03-13 17:33:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,17 @@ var desc = "Watch this amazing video about " + topic;
 
 var tags = topic + ", youtube video, trending";
 
+var idea = "Create an interesting video explaining " + topic;
+
+var script = "Today we are talking about " + topic + ". This topic is very interesting and many people search for it on YouTube.";
+
 document.getElementById("title").innerText = title;
 document.getElementById("desc").innerText = desc;
 document.getElementById("tags").innerText = tags;
+document.getElementById("idea").innerText = idea;
+document.getElementById("script").innerText = script;
 
 }
+
 
 
