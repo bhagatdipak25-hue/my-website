5caa5a0e838ff29da021c6ffaebb50dcf5051c0b
Dipak bhagat <bhagatdipak25@gmail.com>
2026-03-13 16:48:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-unction generate(){
+function generate(){
 
-let topic = document.getElementById("topic").value;
+var topic = document.getElementById("topic").value;
 
-let titles = [
+if(topic == ""){
+alert("Please enter topic");
+return;
+}
+
+var titles = [
 topic + " | Amazing Video",
 topic + " | Must Watch",
 topic + " | Viral Video",
 topic + " | Best Moments"
 ];
 
-let randomTitle = titles[Math.floor(Math.random()*titles.length)];
+var randomTitle = titles[Math.floor(Math.random()*titles.length)];
 
-let desc = "Watch this amazing video about " + topic + ". Like, share and subscribe for more.";
+var desc = "Watch this amazing video about " + topic + ". Like and subscribe.";
 
-let tags = topic + ", youtube video, trending video";
+var tags = topic + ", youtube video, trending video";
 
 document.getElementById("title").innerText = randomTitle;
 document.getElementById("desc").innerText = desc;
 document.getElementById("tags").innerText = tags;
 
-}
+
 
